99e21560a1251e8b4cdf3c40098660db0f3e059e
Niugnep <penguin@niugnep.idv.tw>
2026-08-12 16:44:01 +0800

    feat: allow app read config; move api_key to config.yaml

---
+++ main.py
@@ -0,0 +1,14 @@
+import lib.yaml as yaml
+import lib.markdown2 as md
+import subprocess
+from pathlib import Path
+
+config_file = Path('config.yaml')
+config = yaml.safe_load('''editor: vim''')
+if config_file.exists():
+    with open('config.yaml', 'r') as f:
+        config = yaml.safe_load(f)
+else:
+    print('\'config.yaml\' not found, use default config.')
+
+print('Welcome to Niugnep\'s Resend Mailer!')
